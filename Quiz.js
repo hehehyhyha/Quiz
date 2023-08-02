0f68e098824b0b5ec5db668d098a73767fb8996d
dababyletgo <belloolajide1@gmail.com>
2023-08-02 18:10:48 +0100

    My commit

---
+++ Quiz.js
@@ -0,0 +1,32 @@
+function changeColor() { 
+    var inputtedText = document.getElementById("myText").value
+        //alert(inputtedText);
+    if(inputtedText == "Washington"){
+    document.getElementById("Question1").style.color = "green";
+    }
+    else{
+        document.getElementById("Question1").style.color = "red"; 
+    }
+
+     if(inputtedText == "Whale"){
+        document.getElementById("Question2").style.color = "green";
+        }
+        else{
+            document.getElementById("Question2").style.color = "red"; 
+        }
+
+  
+        if(inputtedText == "Joe Biden"){
+        document.getElementById("Question3").style.color = "green";
+        }
+        else{
+            document.getElementById("Question3").style.color = "red"; 
+        }
+
+        if(inputtedText == "Tinubu"){
+        document.getElementById("Question4").style.color = "green";
+        }
+        else{
+            document.getElementById("Question4").style.color = "red"; 
+        }
+}
